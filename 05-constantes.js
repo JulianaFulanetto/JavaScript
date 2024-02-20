9ca496ca371eff8c9344a6ef3e258aab9093ff3c
Juliana <julianafulanetto.senai@gmail.com>
2024-02-20 14:03:04 -0300

    EntregaAtividade2

---
+++ 05-constantes.js
@@ -0,0 +1,19 @@
+// const x = 10;
+// var y = 20;
+
+// console.log(x + y);
+
+// x = 3;
+// console.log(x + y);
+
+
+//Criando constantes com "const" (const nome_constante = <valor>)
+//Atividade de cálculo e do comprimento de um circunferência 
+
+const pi = 3.14159265359 
+var raio = 5
+
+console.log("Comprimento de uma circunferência de raio "+ raio + "cm =", 2*pi*raio)
+
+console.log("tentando alterar o valor de pi")
+// pi=10
---
+++ Atividade2-Juliana.js
@@ -0,0 +1,41 @@
+//ex.01
+var nome = "Juliana"
+console.log("NOME:", nome);
+
+var ano = "2008"
+console.log("Ano de nascimento", ano);
+
+console.log(nome, "nasceu em", ano);
+
+var atual = 2024
+var nasc = 2008
+console.log("Idade:", atual - nasc);
+
+console.log("Juliana tem", atual - nasc, "anos"); 
+//ex.02
+console.log("------------") 
+var compri = 12
+var larg = 6
+console.log("A área do retangulo é:", compri * larg)
+//ex.03
+var altura = 5
+console.log("O volume é:", compri * larg * altura)
+
+//ex.04
+var a = 2
+var b = 3
+var c = -1
+console.log("o valor de delta é:", b**2 - 4 * a * c)
+
+//ex.05
+var desc = 3
+var valor = 450
+var valDesc = valor*desc/100
+console.log(valDesc)
+console.log("O valor do desconto é R$"+valDesc,"e o valor total da compra é R$", valor-valDesc)
+
+//ex06
+var t = 3.5
+var s = 95
+console.log("a distância percorrida é:", s / t)
+
