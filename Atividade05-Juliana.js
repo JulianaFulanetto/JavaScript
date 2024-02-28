464dbe3ef2294eae13918a9804388c6544b6a965
Juliana <julianafulanetto.senai@gmail.com>
2024-02-28 10:25:10 -0300

    função

---
+++ Atividade05-Juliana.js
@@ -0,0 +1,15 @@
+function idade(ano) {
+  return 2024-ano;
+}
+console.log(`a pessoa tem ${idade(2008)}`);
+
+function quantidade(combustivel) {
+    return combustivel * 12;
+  }
+  console.log(`o carro de jorge pode andar ${quantidade(1, 12)} km`);
+  
+
+function quantidade(combustivel, km) {
+  return combustivel * km;
+}
+console.log(`o carro de jorge pode andar ${quantidade(7, 12)} km`);
